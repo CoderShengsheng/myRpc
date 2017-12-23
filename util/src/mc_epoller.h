34e1a3b900541b2d4cf9b939360eca210e7b5efd
root <root@wangyake.develop.xesv5.com>
2017-12-23 16:06:25 +0800

    up

--- util/src/mc_epoller.h
+++ util/src/mc_epoller.h
@@ -5,12 +5,34 @@
 #ifndef MYRPC_MC_EPOLLER_H
 #define MYRPC_MC_EPOLLER_H
 
+    class MC_Epoller{
+
+    public:
+        MC_Epoller(bool bEt= true);
+        ~MC_Epoller();
+
+        create(int max_connections);
+        add(int fd,long long data, uint32 event);
+        mod(int fd,long long data, uint32 event);
+        del(int fd,long long data,uint32 event);
+        wait(int millsecond);
+
+        struct eppll_event& get(int i){
+
+        }
 
 
-class mc_epoller {
+    protected:
 
-};
+        int _iEpollfd;
 
+        int _max_connections;
+
+        struct epoll_event *_pevs;
+
+        bool _et;
+
+    };
 
 
 #endif //MYRPC_MC_EPOLLER_H
